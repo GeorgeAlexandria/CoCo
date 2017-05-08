714fa6c2fc6b22cfb23f4a2ae1f43e2cebf197ed
George Alexandria <giorgi.alexandria@gmail.com>
2017-05-09 01:29:30 +0300

    Move log files to local application data.

--- CoCo/NLog.cs
+++ CoCo/NLog.cs
@@ -1,4 +1,6 @@
-﻿using NLog;
+﻿using System;
+using System.IO;
+using NLog;
 using NLog.Config;
 using NLog.Targets;
 
@@ -8,15 +10,21 @@ namespace CoCo
     {
         internal static void Initialize()
         {
+            string appDataLocal = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\CoCo";
+            if (!Directory.Exists(appDataLocal))
+            {
+                Directory.CreateDirectory(appDataLocal);
+            }
+
             LoggingConfiguration config = new LoggingConfiguration();
             FileTarget fileTarget = new FileTarget("File");
             FileTarget fileDebugTarget = new FileTarget("File debug");
 
             fileTarget.Layout = "${date}___${level}___${message}";
-            fileTarget.FileName = @"${nlogdir}\file.log";
+            fileTarget.FileName = $"{appDataLocal}\\file.log";
 
             fileDebugTarget.Layout = "${date}___${level}___${message}${newline}${stacktrace}";
-            fileDebugTarget.FileName = @"${nlogdir}\file_debug.log";
+            fileDebugTarget.FileName = $"{appDataLocal}\\file_debug.log";
 
             config.AddTarget(fileTarget);
             config.AddTarget(fileDebugTarget);
