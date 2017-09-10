cc480bd590afc3a38c60d94b4dcc01253ced6ad7
George Alexandria <giorgi.alexandria@gmail.com>
2017-09-10 20:40:31 +0300

    Continue adding a stubs.

---
+++ CoCoTests/TrackingPoint.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.Text;
+
+namespace CoCoTests
+{
+    internal class TrackingPoint : ITrackingPoint
+    {
+        private ITextVersion _textVersion;
+        private int _position;
+
+        internal TrackingPoint(ITextVersion version, int position, PointTrackingMode trackingMode)
+        {
+            if (version == null)
+            {
+                throw new ArgumentNullException(nameof(version));
+            }
+            if (position < 0 || position > version.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(position));
+            }
+            if (trackingMode < PointTrackingMode.Positive || trackingMode > PointTrackingMode.Negative)
+            {
+                throw new ArgumentOutOfRangeException(nameof(trackingMode));
+            }
+
+            _textVersion = version;
+            _position = position;
+
+            TrackingMode = trackingMode;
+        }
+
+        public ITextBuffer TextBuffer { get; }
+
+        public PointTrackingMode TrackingMode { get; }
+
+        public TrackingFidelityMode TrackingFidelity { get; }
+
+        public int GetPosition(ITextVersion version)
+        {
+            if (version == null)
+            {
+                throw new ArgumentNullException(nameof(version));
+            }
+            if (version.TextBuffer != TextBuffer)
+            {
+                throw new ArgumentException();
+            }
+
+            return TrackPosition(version);
+        }
+
+        public int GetPosition(ITextSnapshot snapshot)
+        {
+            if (snapshot == null)
+            {
+                throw new ArgumentNullException(nameof(snapshot));
+            }
+            if (snapshot.TextBuffer != TextBuffer)
+            {
+                throw new ArgumentException();
+            }
+
+            return TrackPosition(snapshot.Version);
+        }
+
+        public SnapshotPoint GetPoint(ITextSnapshot snapshot) => new SnapshotPoint(snapshot, GetPosition(snapshot));
+
+        public char GetCharacter(ITextSnapshot snapshot) => GetPoint(snapshot).GetChar();
+
+        private int TrackPosition(ITextVersion targetVersion)
+        {
+            if (_textVersion == targetVersion)
+            {
+                return _position;
+            }
+            if (targetVersion.VersionNumber > _textVersion.VersionNumber)
+            {
+                _position = Tracking.TrackPositionForwardInTime(TrackingMode, _position, _textVersion, targetVersion);
+                _textVersion = targetVersion;
+            }
+            else
+            {
+                _position = Tracking.TrackPositionForwardInTime(TrackingMode, _position, _textVersion, targetVersion);
+            }
+            return _position;
+        }
+    }
+}
