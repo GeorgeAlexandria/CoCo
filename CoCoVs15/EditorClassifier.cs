e4cb2677679822578473aec1a393a30864273233
George Alexandria <giorgi.alexandria@gmail.com>
2018-03-31 21:25:56 +0300

    Avoid to use the different version of System.Collections.Immutable

--- CoCoVs15/EditorClassifier.cs
+++ CoCoVs15/EditorClassifier.cs
@@ -126,7 +126,7 @@ namespace CoCo
                     // TODO: Log information about a node and semantic model, because semantic model
                     // didn't retrive information from node in this case
                     Log.Debug("Nothing is found. Span start at {0} and end at {1}", span.Start.Position, span.End.Position);
-                    Log.Debug("Candidate Reason {0}. Candidate counts {1}", info.CandidateReason, info.CandidateSymbols.Length);
+                    Log.Debug("Candidate Reason {0}", info.CandidateReason);
                     Log.Debug("Node is {0}", node);
                     continue;
                 }
