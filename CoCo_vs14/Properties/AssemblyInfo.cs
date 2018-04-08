19c71c1e5c9772582ab85fd7b8a8a19933da16e4
George Alexandria <giorgi.alexandria@gmail.com>
2018-04-08 15:33:38 +0300

    Renamed project from the year of visual studio 2015 to the version

--- CoCo_vs14/Properties/AssemblyInfo.cs
+++ CoCo_vs14/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CoCoVs15")]
+[assembly: AssemblyTitle("CoCo_vs14")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
