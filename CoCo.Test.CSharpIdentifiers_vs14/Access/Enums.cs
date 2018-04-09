80036a763d8899aee62d28301eb6b8ee20d785c1
George Alexandria <giorgi.alexandria@gmail.com>
2018-04-09 22:41:44 +0300

    Added base access tests for vs14.

---
+++ CoCo.Test.CSharpIdentifiers_vs14/Access/Enums.cs
@@ -0,0 +1,17 @@
+﻿using CoCo.Test.Common;
+using NUnit.Framework;
+
+namespace CoCo.Test.CSharpIdentifiers.Access
+{
+    internal class Enums : CSharpIdentifierTests
+    {
+        [Test]
+        public void EnumTest()
+        {
+            @"Tests\CSharpIdentifiers\CSharpIdentifiers\Access\EnumField.cs".GetClassifications(ProjectInfo)
+                .AssertContains(
+                    Names.EnumFieldName.ClassifyAt(152, 9),
+                    Names.EnumFieldName.ClassifyAt(209, 3));
+        }
+    }
+}
