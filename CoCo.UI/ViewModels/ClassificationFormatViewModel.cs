9297a057b2546c9d089bd7b87b80048f6fd60bfe
George Alexandria <giorgi.alexandria@gmail.com>
2018-04-15 15:18:30 +0300

    Added implementation of ICommand. Added function to choice a color.

--- CoCo.UI/ViewModels/ClassificationFormatViewModel.cs
+++ CoCo.UI/ViewModels/ClassificationFormatViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Forms;
 using System.Windows.Media;
 
 namespace CoCo.UI.ViewModels
@@ -13,6 +14,24 @@ namespace CoCo.UI.ViewModels
 
     public class ClassificationFormatViewModel : BaseViewModel
     {
+        public ClassificationFormatViewModel()
+        {
+            CustomizeForeground = new DelegateCommand(() =>
+            {
+                if (TryGetColor(out var color))
+                {
+                    Foreground = color;
+                }
+            });
+            CustomizeBackground = new DelegateCommand(() =>
+            {
+                if (TryGetColor(out var color))
+                {
+                    Background = color;
+                }
+            });
+        }
+
         public bool IsCheked { get; set; } = true;
 
         public string Name { get; set; } = "Sample";
@@ -27,6 +46,37 @@ namespace CoCo.UI.ViewModels
                 _foreground = value;
                 RaisePropertyChanged();
             }
+        }
+
+        private Color _background = Color.FromRgb(128, 128, 128);
+
+        public Color Background
+        {
+            get => _background;
+            set
+            {
+                _background = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DelegateCommand CustomizeForeground { get; }
+
+        public DelegateCommand CustomizeBackground { get; }
+
+        // TODO: would be a better solution to implement a custom color picker in wpf...
+        // or move all logic of setter color from button to control code behind.
+        private bool TryGetColor(out Color color)
+        {
+            var dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var outColor = dialog.Color;
+                color = Color.FromArgb(outColor.A, outColor.R, outColor.G, outColor.B);
+                return true;
+            }
+            color = default(Color);
+            return false;
         }
     }
 }
