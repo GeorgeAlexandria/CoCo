698a36596a9a08ccf7641f983cadb191ac7b5a32
George Alexandria <giorgi.alexandria@gmail.com>
2018-06-10 13:05:16 +0300

    Added vs package.

---
+++ CoCo/VsPackage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using Microsoft.VisualStudio.Shell;
+
+namespace CoCo
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading =true)]
+    [Guid("b933474d-306e-434f-952d-a820c849ed07")]
+    public sealed class VsPackage : Package
+    {
+    }
+}
