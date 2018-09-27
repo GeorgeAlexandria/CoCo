2b659c48e487970dec803735c3c51d647d8430d0
George Alexandria <giorgi.alexandria@gmail.com>
2018-09-27 22:58:56 +0300

    Updated version.

--- Tests/AssemblyInfoCommon.vb
+++ Tests/AssemblyInfoCommon.vb
@@ -6,8 +6,8 @@ Imports System.Runtime.InteropServices
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 <Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("VisualBasicIdentifiers")>
-<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyProduct("CoCo")>
+<Assembly: AssemblyCopyright("Copyright © 2018 George Aleksandria")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -19,5 +19,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
-<Assembly: AssemblyVersion("2.2.0.0")>
-<Assembly: AssemblyFileVersion("2.2.0.0")>
+<Assembly: AssemblyVersion("2.3.0.0")>
+<Assembly: AssemblyFileVersion("2.3.0.0")>
