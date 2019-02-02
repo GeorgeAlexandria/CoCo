695667795dd3e0a54f6ca822c5ff1edad9a5f35c
George Alexandria <giorgi.alexandria@gmail.com>
2019-02-02 19:01:05 +0300

    Start implementing the custom quick info source.

---
+++ src/vs15/CoCo.Analyser/CSharp/QuickInfoSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+
+namespace CoCo.Analyser.CSharp
+{
+    internal sealed class QuickInfoSource : IAsyncQuickInfoSource
+    {
+        private readonly ITextBuffer textBuffer;
+
+        public QuickInfoSource(ITextBuffer textBuffer)
+        {
+            this.textBuffer = textBuffer;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public async Task<QuickInfoItem> GetQuickInfoItemAsync(IAsyncQuickInfoSession session, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/vs15/CoCo/Providers/QuickInfoProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Composition;
+using CoCo.Analyser.CSharp;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+
+namespace CoCo.Providers
+{
+    [Export(typeof(IAsyncQuickInfoSourceProvider))]
+    [ContentType("any")]
+    internal sealed class QuickInfoProvider : IAsyncQuickInfoSourceProvider
+    {
+        public IAsyncQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
+        {
+            return textBuffer.Properties.GetOrCreateSingletonProperty(() => new QuickInfoSource(textBuffer));
+        }
+    }
+}
