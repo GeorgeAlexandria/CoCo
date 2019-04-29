ea2f197c1d922544ad69de463e7184190a2af473
George Alexandria <giorgi.alexandria@gmail.com>
2019-04-29 22:43:47 +0300

    Fix names for vs15

--- src/vs15/CoCo.Analyser_vs15/Properties/AssemblyInfo.cs
+++ src/vs15/CoCo.Analyser_vs15/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("CoCo")]
-[assembly: InternalsVisibleTo("CoCo.Test.Common")]
+[assembly: InternalsVisibleTo("CoCo_vs15")]
+[assembly: InternalsVisibleTo("CoCo.Test.Common_vs15")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
